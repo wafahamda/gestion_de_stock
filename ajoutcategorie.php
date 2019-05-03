88979f6af114bab86a4bb6a1987f9d94b50ddac0
hamdi.hatem <hamdii.hatem@msstn.net>
2019-05-03 17:57:07 +0100

    ajout categorie

---
+++ ajoutcategorie.php
@@ -0,0 +1,33 @@
+<body bgcolor="#CCCCCC">
+<div align="center">
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp; </p>
+    <p><a href="ajout.html">Retour a la page precedante</a></p>
+  </div>
+
+ 
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gestion_stock";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+ 
+
+  $sql = "INSERT  INTO categorie (id,  nom)
+            VALUES ( '1', 'wafa' ) " ;
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
